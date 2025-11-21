227bc6a61ab9fafbbfd018697f2f02b9c5326404
chz <84703732@qq.com>
2025-11-21 15:21:36 +0800

    feat Toast 通知组件

--- App.tsx
+++ App.tsx
@@ -2,9 +2,15 @@
 import React from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
+import ToastContainer from './components/Toast';
 
 const App: React.FC = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <RouterProvider router={router} />
+      <ToastContainer />
+    </>
+  );
 };
 
 export default App;
